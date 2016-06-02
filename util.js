738176e1b7080e66a63cc96bc57e7103d74054d4
Jimmy Wärting <jimmy@warting.se>
2016-06-03 00:35:34 +0200

    added blob example

--- util.js
+++ util.js
@@ -4,21 +4,40 @@ window.blob2Stream = blob =>
 	.then(res => pump(res.body.getReader()))
 
 // Or do you want to read it yourself?
-window.blob2reader = (blob, callback, chunkSize = 524288) => {
+// This is not nearly as good as stream as it can not handle backpressure
+window.blob2reader = (blob, callback, highWaterMark = 524288) => {
 	let
-	chunks = Math.ceil(blob.size / chunkSize),
+	chunks = Math.ceil(blob.size / highWaterMark),
 	currentChunk = 0,
 	fr = new FileReader()
 
 	fr.onload = function(e) {
 		currentChunk++
-		callback({value: e.target.result, done: currentChunk == chunks})
-		if(currentChunk == chunks) return
-
-		let start = currentChunk * chunkSize
-		let end = ((start + chunkSize) >= blob.size) ? blob.size : start + chunkSize
-		fr.readAsArrayBuffer(blob.slice(start, end))
+		callback({
+			value: e.target.result,
+			done: currentChunk >= chunks,
+			next(){
+				let start = currentChunk * highWaterMark
+				let end = ((start + highWaterMark) >= blob.size) ? blob.size : start + highWaterMark
+				fr.readAsArrayBuffer(blob.slice(start, end))
+			}
+		})
 	}
 	// kick off the reading of the file
-	fr.readAsArrayBuffer(blob.slice(0, chunkSize))
+	fr.readAsArrayBuffer(blob.slice(0, highWaterMark))
 }
+
+// // This is a bit more unfinish advance handle backpressure
+// window.blob2StreamX = (blob, highWaterMark)
+//
+// new ReadableStream({
+// 	start(controller) {
+// 		blob2reader
+// 	},
+// 	pull(controller) {
+//
+// 	},
+// 	cancel() {
+// 		console.log("user aborted")
+// 	}
+// })
